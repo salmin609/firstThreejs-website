d7f7045bf77ab6c6f3fbda57f595c6b6f39a9e77
salmin609 <ryan95kr@gmail.com>
2023-09-30 10:29:38 +0900

    Fixing

--- JavaScripts/08-AnimModel.js
+++ JavaScripts/08-AnimModel.js
@@ -1,5 +1,5 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
-import {_Math} from "MathFile";
+import * as THREE from 'three';
+import {_Math} from 'MathFile';
 import {OrbitControls} from "OrbitControlFile";
 import {GLTFLoader} from "GLTFFile";
 import {AnimationMixer} from "AnimationMixerFile";
